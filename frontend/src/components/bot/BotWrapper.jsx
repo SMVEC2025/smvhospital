391d8d96ffe2ee8be0065d4c3ccd9b57d5ef2e38
premkumar <premkumarananthan1@gmail.com>
2025-02-19 17:50:28 +0530

    creating bot wrapper

---
+++ frontend/src/components/bot/BotWrapper.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import '../../styles/BotWrapper.css'
+function BotWrapper() {
+  return (
+    <div className='bw_main'>
+
+    </div>
+  )
+}
+
+export default BotWrapper
