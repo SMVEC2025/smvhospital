1dbf97dc6480162a22bfb1616a42f27521539b3f
premkumar <premkumarananthan1@gmail.com>
2025-02-25 16:45:15 +0530

    mobile home

--- frontend/src/components/navbar/Navbar.jsx
+++ frontend/src/components/navbar/Navbar.jsx
@@ -1,50 +1,67 @@
-import React, { useEffect, useState } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import '../../styles/Navbar.css'
 import { IoAppsSharp } from "react-icons/io5";
+import { AppContext } from '../../context/AppContext';
 import logofull from '../../assets/images/logofull.png'
 function Navbar() {
-    const [scrolled, setScrolled] = useState(false);
-    const [showNav,setShowNav] = useState(false)
+  const [scrolled, setScrolled] = useState(false);
+  const [showNav, setShowNav] = useState(false)
+  const { isMobile } = useContext(AppContext)
 
-    useEffect(() => {
-      const handleScroll = () => {
-        if (window.scrollY > 50) {
-          setScrolled(true);
-        } else {
-          setScrolled(false);
-        }
-      };
-  
-      window.addEventListener("scroll", handleScroll);
-      return () => window.removeEventListener("scroll", handleScroll);
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 50) {
+        setScrolled(true);
+      } else {
+        setScrolled(false);
+      }
+    };
 
-      
-    }, []);
-    useEffect(() => {
-      setTimeout(() => {
-        setShowNav(true)
-      }, 6000);
-    }, [])
-    
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+
+
+  }, []);
+  useEffect(() => {
+    setTimeout(() => {
+      setShowNav(true)
+    }, 6000);
+  }, [])
+
   return (
     <>
-    {showNav?(<div className={`nav_main ${scrolled}`}>
+      {showNav ? (<div className={`nav_main ${scrolled}`}>
         <div className='ns_con1'>
-            <img src={logofull} alt="" />
+          <img src={logofull} alt="" />
         </div>
-        <div className='ns_con2'>
-            <span>Home</span>
-            <span>About us</span>
-            <span>Treatments</span>
-            <span>Speacialty</span>
-            <span>Contact us</span>
+        {isMobile ? (
+          <div className='mobilemenudiv'>
 
-        </div>
-    
-        <div className='ns_con3'>
-             <span>EMERGENCY</span>
-        </div>
-    </div>):null}
+            <input id="burger-checkbox" type="checkbox" />
+            <label class={`burger ${scrolled}`} for="burger-checkbox">
+              <span></span>
+              <span></span>
+              <span></span>
+            </label>
+          </div>
+        ) : (
+          <>
+            <div className='ns_con2'>
+              <span>Home</span>
+              <span>About us</span>
+              <span>Treatments</span>
+              <span>Speacialty</span>
+              <span>Contact us</span>
+
+            </div>
+
+            <div className='ns_con3'>
+              <span>EMERGENCY</span>
+            </div>
+          </>
+        )}
+
+      </div>) : null}
     </>
   )
 }
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -16,7 +16,7 @@ function Home() {
   return (
     <div>
         <BotWrapper/>
-        {/* <Navbar/> */}
+        <Navbar/>
         <Hero/>
         <HomeAbout/>
         <LedContainer/>
