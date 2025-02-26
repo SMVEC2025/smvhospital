8c6f38eb95e814a4bce6d30fe981ae904b60eed5
premkumar <premkumarananthan1@gmail.com>
2025-02-26 15:57:18 +0530

    changes

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,12 +1,13 @@
 import React, { Suspense } from "react";
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { LuLoaderCircle } from "react-icons/lu";
 import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Suspense fallback={<div  className="loader_main"><img src="/smvloader.svg" alt="" /></div>}>
+    <Suspense fallback={<div  className="loader_main"><span><LuLoaderCircle/></span></div>}>
 
     <App />
     </Suspense>
