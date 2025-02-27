7d526cfd51fa38ccd5f1de02d1a2ef91cb7284f8
premkumar <premkumarananthan1@gmail.com>
2025-02-27 15:24:48 +0530

    changes

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,20 +4,7 @@ import { createRoot } from 'react-dom/client'
 import { LuLoaderCircle } from "react-icons/lu";
 import './index.css'
 import App from './App.jsx'
-import axios from 'axios';
 
-const api = axios.create({
-  baseURL: 'smvhospital-3p78.vercel.app/api',
-});
-
-// Export the Axios instance
-export { api };
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Suspense fallback={<div  className="loader_main"><span><LuLoaderCircle/></span></div>}>
