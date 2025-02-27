649082974d3551025785122d0a3ef6374b04046e
premkumar <premkumarananthan1@gmail.com>
2025-02-27 15:38:36 +0530

    changes

--- backend/routes/formRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import { createForm } from '../controllers/formController.js';
-
-const router = express.Router();
-
-router.post('/submit-form', createForm);
-
-export default router;
