d8501d67ae9ebdf12b93a2e3c1dd7c05d1fc97a5
PREMKUMAR5519 <premkumarananthan1@gmail.com>
2025-02-28 09:06:29 +0530

    changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,22 @@
-import React from 'react'
-import Home from './pages/Home'
-import { AppProvider } from './context/AppContext'
-import './App.css'
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import { AppProvider } from "./context/AppContext";
+import "./App.css";
 import "./i18n";
 
 function App() {
   return (
-    <>
     <AppProvider>
-    <Home/>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </Router>
     </AppProvider>
-    </>
-  )
+  );
 }
 
-export default App
+export default App;
