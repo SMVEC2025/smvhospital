7284cfac7d4577d5a33248238b639f1f26d9ec18
PREMKUMAR5519 <premkumarananthan1@gmail.com>
2025-04-04 16:53:21 +0530

    changes

--- frontend/src/components/LogoAnimation.jsx
+++ frontend/src/components/LogoAnimation.jsx
@@ -1,21 +1,30 @@
 import React, { useState, useEffect } from "react";
 import frames from "../frames/Frames";
-import '../styles/LogoAnimation.css'
+import '../styles/LogoAnimation.css';
+
 const LogoAnimation = () => {
   const [currentFrame, setCurrentFrame] = useState(0);
 
   useEffect(() => {
+    if (currentFrame >= frames.length - 1) return; // Stop if last frame reached
+
     const frameInterval = setInterval(() => {
-      setCurrentFrame((prevFrame) => (prevFrame + 1) % frames.length);
-    }, 50); // Adjust speed (lower = faster)
+      setCurrentFrame((prevFrame) => {
+        if (prevFrame >= frames.length - 1) {
+          clearInterval(frameInterval); // Clear interval when last frame is reached
+          return prevFrame;
+        }
+        return prevFrame + 1;
+      });
+    }, 30);
 
     return () => clearInterval(frameInterval);
-  }, []);
+  }, [currentFrame]); // Re-run effect only when currentFrame changes
 
   const FrameComponent = frames[currentFrame];
 
-  return ( 
-    <div className="logoanim" >
+  return (
+    <div className="logoanim">
       <FrameComponent />
     </div>
   );
