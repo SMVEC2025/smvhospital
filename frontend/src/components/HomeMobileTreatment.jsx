4898a77236be4cf8208f317b041954bb662ad30a
premkumar <premkumarananthan1@gmail.com>
2025-04-10 16:57:27 +0530

    changes

--- frontend/src/components/HomeMobileTreatment.jsx
+++ frontend/src/components/HomeMobileTreatment.jsx
@@ -12,75 +12,75 @@ import '../styles/HomeMobileTreatment.css';
 import { Pagination } from 'swiper/modules';
 import TreatmentCard from './TreatmentCard';
 const treatments = [
-    {
-      id: 1,
-      name: "Liver Transplant",
-      desc: "A surgical procedure to replace a diseased liver with a healthy donor liver.",
-      image: "https://img.freepik.com/free-photo/doctors-doing-surgical-procedure-patient_23-2148962498.jpg?uid=R184073455&ga=GA1.1.33103983.1738217368&semt=ais_hybrid&w=740"
-    },
-    {
-      id: 2,
-      name: "Heart Transplant",
-      desc: "Replacement of a failing heart with a healthy donor heart to restore normal function.",
-      image: "https://img.freepik.com/free-photo/surgeons-performing-operation-operation-room_1170-2232.jpg?uid=R184073455&ga=GA1.1.33103983.1738217368&semt=ais_hybrid&w=740"
-    },
-    {
-      id: 3,
-      name: "Kidney Transplant",
-      desc: "Surgical placement of a healthy kidney from a donor to treat kidney failure.",
-      image: "https://img.freepik.com/free-photo/healthcare-concept-clinic_23-2151117937.jpg?uid=R184073455&ga=GA1.1.33103983.1738217368&semt=ais_hybrid&w=740"
-    },
-    {
-      id: 4,
-      name: "Heart Valve Repair / Replacement",
-      desc: "Surgery to fix or replace damaged heart valves to improve blood flow.",
-      image: "https://img.freepik.com/free-photo/view-anatomic-heart-model-educational-purpose-with-medical-instruments_23-2149894480.jpg?uid=R184073455&ga=GA1.1.33103983.1738217368&semt=ais_hybrid&w=740"
-    },
-  
-    {
-      id: 5,
-      name: "PTCA",
-      desc: "A non-surgical procedure to open blocked coronary arteries using a balloon catheter.",
-      image: "https://img.freepik.com/free-photo/doctor-getting-patient-ready-ct-scan_23-2149367411.jpg?uid=R184073455&ga=GA1.1.33103983.1738217368&semt=ais_hybrid&w=740"
-    },
-    {
-      id: 6,
-      name: "Joint Replacement",
-      desc: "Surgical replacement of damaged joints with artificial implants to relieve pain.",
-      image: "https://img.freepik.com/free-photo/old-woman-nursing-home-walking-with-crutches-with-help-from-female-nurse_482257-45331.jpg?uid=R184073455&ga=GA1.1.33103983.1738217368&semt=ais_hybrid&w=740"
-    },
-    {
-      id: 7,
-      name: "Spine Surgery",
-      desc: "Surgical treatment for spine-related disorders such as herniated discs or scoliosis.",
-      image: "https://img.freepik.com/free-photo/osteopathy-specialist-relieving-pressure-from-back-bones-aged-patient-physiotherapy-chiropractor-giving-support-helping-senior-woman-with-spinal-cord-ache-physical-recovery_482257-31871.jpg?uid=R184073455&ga=GA1.1.33103983.1738217368&semt=ais_hybrid&w=740"
-    },
-    {
-      id: 8,
-      name: "Plastic and Reconstructive Surgery",
-      desc: "Surgery to restore appearance and function due to injury, disease, or birth defects.",
-      image: "https://img.freepik.com/free-photo/doctor-drawing-with-marker-face-high-angle_23-2149352181.jpg?uid=R184073455&ga=GA1.1.33103983.1738217368&semt=ais_hybrid&w=740"
-    },
-    {
-      id: 9,
-      name: "Coronary Artery Bypass Grafting (CABG)",
-      desc: "A procedure to bypass blocked heart arteries and improve blood flow.",
-      image: "https://img.freepik.com/free-photo/person-holding-anatomic-heart-model-educational-purpose_23-2149894442.jpg?uid=R184073455&ga=GA1.1.33103983.1738217368&semt=ais_hybrid&w=740"
-    },
-    {
-      id: 10,
-      name: "Cosmetology",
-      desc: "Non-surgical and surgical procedures to enhance or restore physical appearance.",
-      image: "https://img.freepik.com/free-photo/young-woman-having-facial-treatment_23-2148906470.jpg?uid=R184073455&ga=GA1.1.33103983.1738217368&semt=ais_hybrid&w=740"
-    },
-    {
-      id: 11,
-      name: "Podiatrics",
-      desc: "Medical and surgical care focused on foot, ankle, and lower limb disorders.",
-      image: "https://img.freepik.com/free-photo/male-doctor-bandaging-foot-female-patient_1170-2174.jpg?uid=R184073455&ga=GA1.1.33103983.1738217368&semt=ais_hybrid&w=740"
-    }
-  
-  ]
+  {
+    id: 1,
+    name: "Liver Transplant",
+    desc: "A surgical procedure to replace a diseased liver with a healthy donor liver.",
+    image: "https://img.freepik.com/free-photo/doctors-doing-surgical-procedure-patient_23-2148962498.jpg?uid=R184073455&ga=GA1.1.33103983.1738217368&semt=ais_hybrid&w=740"
+  },
+  {
+    id: 2,
+    name: "Heart Transplant",
+    desc: "Replacement of a failing heart with a healthy donor heart to restore normal function.",
+    image: "https://images.pexels.com/photos/4483323/pexels-photo-4483323.jpeg?auto=compress&cs=tinysrgb&w=600"
+  },
+  {
+    id: 3,
+    name: "Kidney Transplant",
+    desc: "Surgical placement of a healthy kidney from a donor to treat kidney failure.",
+    image: "https://img.freepik.com/free-photo/medium-shot-doctor-talking-patient_23-2149352164.jpg?uid=R184073455&ga=GA1.1.33103983.1738217368&semt=ais_hybrid&w=740"
+  },
+  {
+    id: 4,
+    name: "Heart Valve Repair / Replacement",
+    desc: "Surgery to fix or replace damaged heart valves to improve blood flow.",
+    image: "https://img.freepik.com/free-photo/view-anatomic-heart-model-educational-purpose-with-medical-instruments_23-2149894480.jpg?uid=R184073455&ga=GA1.1.33103983.1738217368&semt=ais_hybrid&w=740"
+  },
+
+  {
+    id: 5,
+    name: "PTCA",
+    desc: "A non-surgical procedure to open blocked coronary arteries using a balloon catheter.",
+    image: "https://img.freepik.com/free-photo/doctor-getting-patient-ready-ct-scan_23-2149367411.jpg?uid=R184073455&ga=GA1.1.33103983.1738217368&semt=ais_hybrid&w=740"
+  },
+  {
+    id: 6,
+    name: "Joint Replacement",
+    desc: "Surgical replacement of damaged joints with artificial implants to relieve pain.",
+    image: "https://img.freepik.com/free-photo/old-woman-nursing-home-walking-with-crutches-with-help-from-female-nurse_482257-45331.jpg?uid=R184073455&ga=GA1.1.33103983.1738217368&semt=ais_hybrid&w=740"
+  },
+  {
+    id: 7,
+    name: "Spine Surgery",
+    desc: "Surgical treatment for spine-related disorders such as herniated discs or scoliosis.",
+    image: "https://img.freepik.com/free-photo/osteopathy-specialist-relieving-pressure-from-back-bones-aged-patient-physiotherapy-chiropractor-giving-support-helping-senior-woman-with-spinal-cord-ache-physical-recovery_482257-31871.jpg?uid=R184073455&ga=GA1.1.33103983.1738217368&semt=ais_hybrid&w=740"
+  },
+  {
+    id: 8,
+    name: "Plastic and Reconstructive Surgery",
+    desc: "Surgery to restore appearance and function due to injury, disease, or birth defects.",
+    image: "https://img.freepik.com/free-photo/doctor-drawing-with-marker-face-high-angle_23-2149352181.jpg?uid=R184073455&ga=GA1.1.33103983.1738217368&semt=ais_hybrid&w=740"
+  },
+  {
+    id: 9,
+    name: "Coronary Artery Bypass Grafting (CABG)",
+    desc: "A procedure to bypass blocked heart arteries and improve blood flow.",
+    image: "https://img.freepik.com/free-photo/person-holding-anatomic-heart-model-educational-purpose_23-2149894442.jpg?uid=R184073455&ga=GA1.1.33103983.1738217368&semt=ais_hybrid&w=740"
+  },
+  {
+    id: 10,
+    name: "Cosmetology",
+    desc: "Non-surgical and surgical procedures to enhance or restore physical appearance.",
+    image: "https://img.freepik.com/free-photo/young-woman-having-facial-treatment_23-2148906470.jpg?uid=R184073455&ga=GA1.1.33103983.1738217368&semt=ais_hybrid&w=740"
+  },
+  {
+    id: 11,
+    name: "Podiatrics",
+    desc: "Medical and surgical care focused on foot, ankle, and lower limb disorders.",
+    image: "https://img.freepik.com/free-photo/male-doctor-bandaging-foot-female-patient_1170-2174.jpg?uid=R184073455&ga=GA1.1.33103983.1738217368&semt=ais_hybrid&w=740"
+  }
+
+]
 export default function HomeMobileTreatment() {
     return (
         <div className='hmt_main'>
